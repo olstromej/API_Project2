3f45ad90e15357d7b3370ac2db893f189a260291
Edward Olstrom <eddieolstrom1@gmail.com>
2022-12-17 14:38:40 -0500

    fixed error on seed file

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import connection from './connection.js'
-import data from './breweries.json' assert { type: 'json '}
+import data from './breweries.json' assert { type: 'json' }
 import breweries from '../models/Breweries.js'
 import chalk from "chalk"
 
