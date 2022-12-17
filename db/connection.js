e40e51b3ecacf2dbf70cd2648299d8855751a6c3
Edward Olstrom <eddieolstrom1@gmail.com>
2022-12-17 13:00:37 -0500

    setting up server

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import chalk from "chalk"
+
+mongoose.set("returnOriginal", false);
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect("mongodb://127.0.0.1:27017/harry-api").catch((err) => {
+  console.log(`Error connection to MongoDB: ${err.message}`);
+});
+
+
+
+
+mongoose.connection.on("disconnected", () => {
+    console.log(chalk.blue("Disconnected from MongoDB!"));
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log(chalk.red(`MongoDB connection error: ${err}`));
+  });
+
+
+export default mongoose.connection;
