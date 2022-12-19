22f0be60feb519d52e5ea28a40b266962ba53b55
Edward Olstrom <eddieolstrom1@gmail.com>
2022-12-18 20:41:22 -0500

    small changes

--- controllers/breweries.js
+++ controllers/breweries.js
@@ -1,8 +1,9 @@
-import Breweries from "../models/Breweries.js";
+import Brewery from "../models/Breweries.js";
 
 export const getBreweries = async (req, res) => {
    try {
-    const Brewery = await Breweries.find();
+    const Brewery = await Brewery.find();
+    res.json(Brewery);
    } catch (error) {
     res.status(500).json({error: error.message});
    }
@@ -11,43 +12,34 @@ export const getBreweries = async (req, res) => {
 export const getBrewery = async (req, res) => {
     try {
         const { id } = req.params
-        const brewery = await Breweries.findById(id)
+        const brewery = await Brewery.findById(id)
 
         if (brewery) {
             return res.json(brewery);
         }
         res.status(404).json({ message: "Brewery not found!" });
        } catch (error) {
+        console.error(error)
         res.status(500).json({error: error.message});
        }
 };
 
 export const createBrewery = async (req, res) => {
-    try {
-        const brewery = new Brewery(req.body);
-        await brewery.save();
-        res.status(201).json(brewery)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message});
-    }
-};
+    const brewery = new Brewery(req.body);
+    await brewery.save();
+    res.status(201).json(brewery)
+}
 
 export const updateBrewery = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const brewery = await Breweries.findByIdAndUpdate(id, req.body)
-        res.status(201).json(brewery)
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message })
-    }
-};
+    const { id } = req.params;
+    const brewery = await Brewery.findByIdAndUpdate(id, req.body);
+    res.status(201).json(brewery)
+}
 
 export const destroyBrewery = async (req, res) => {
     try {
         const { id } = req.params;
-        const destroyed = await Breweries.findByIdAndDelete(id);
+        const destroyed = await Brewery.findByIdAndDelete(id);
 
         if (destroyed) {
             return res.status(200).send("Brewery Destroyed!")
--- db/seed.js
+++ db/seed.js
@@ -1,23 +1,39 @@
-import mongoose from 'mongoose'
-import connection from './connection.js'
+import mongoose, { connect } from 'mongoose'
+import connection from "./connection.js"
 import data from './breweries.json' assert { type: 'json' }
-import breweries from '../models/Breweries.js'
+import Breweries from "../models/Breweries.js"
 import chalk from "chalk"
 
- let breweryData = data.map(item => {
-    const brewery = {}
-    brewery.name = item.name
-    brewery.brewery_type = item.brewery_type
-    brewery.street = item.street
-    brewery.city = item.city
-    brewery.state = item.state
-    return brewery
- })
 
- 
-breweries
-    .deleteMany({})
-    .then(() =>breweries.create(breweryData))
-  .then(() => console.log(chalk.bgBlue.bold("Done! Breweries Created!")))
-  .then(() => mongoose.disconnect())
-  .catch(error => console.log(chalk.red("Error, somethings wrong!", error)))
+
+let breweryData = data.map(item => {
+   const brewery = {}
+   brewery.name = item.name
+   brewery.brewery_type = item.brewery_type
+   brewery.street = item.street
+   brewery.city = item.city
+   brewery.state = item.state
+   return brewery
+})
+
+
+
+
+Breweries
+.deleteMany({})
+.then(() => Breweries.create(breweryData))
+.then(() => console.log(chalk.bgBlue.bold("Done! Breweries Created!")))
+.then(() => mongoose.disconnect())
+.catch(error => console.log(chalk.red("Error, somethings wrong!", error)))
+
+
+
+// const insertData = async () => {
+//     await connection.dropDatabase();
+  
+//     await Breweries.create(data);
+  
+//     await connection.close();
+//   };
+  
+//   insertData();
